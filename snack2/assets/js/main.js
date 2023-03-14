fc0ed51a7e42c30d73be05be6363421e4dd82168
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-14 12:21:26 +0100

    fix

--- snack2/assets/js/main.js
+++ snack2/assets/js/main.js
@@ -1,13 +1,19 @@
 const firstWord = prompt("Inserire una prima Parola")
 const secondWord = prompt("Inserire una seconda Parola")
 
-console.log(firstWord, "Prima parola", secondWord, "Seconda parola")
 console.log(firstWord.length, "Lunghezza Prima Parola", secondWord.length, "Lunghezza Seconda Parola")
 
+if (firstWord === "" || secondWord === "") {
+    alert("Devi inserire una parola!")
+}
+
 if (firstWord.length > secondWord.length) {
-    console.log(`La prima parola ${firstWord} è più lunga della seconda ${secondWord}`)
+    console.log(secondWord.length, firstWord.length)
+    console.log(`La prima parola (${firstWord}) è più lunga della seconda parola (${secondWord})`)
 } else if (secondWord.length > firstWord.length) {
-    console.log(`La seconda parola ${secondWord} è più lunga della prima ${firstWord}`)
+    console.log(firstWord.length, secondWord.length)
+    console.log(`La seconda parola (${secondWord}) è più lunga della prima parola (${firstWord})`)
 } else {
-    console.log(`La prima parola ${secondWord} e la seconda parola ${firstWord} hanno la stessa lunghezza`)
+    console.log(firstWord.length, secondWord.length)
+    console.log(`La prima parola (${firstWord}) e la seconda parola (${secondWord}) hanno la stessa lunghezza`)
 }
