71f85a0312823d9fc5bd0cefee5df16d5a2dfc0c
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-14 12:50:13 +0100

    add snack 3

---
+++ snack3/assets/js/main.js
@@ -0,0 +1,14 @@
+let somma = 0;
+for (let i = 1; i <= 10; i++) {
+  let numero = Number(prompt(`Inserisci il numero ${i}:`));
+  somma += numero;
+  console.log(`Il numero scritto è ${numero}`);
+  console.log(`La somma attuale è ${somma}`);
+  if (isNaN(numero)) {
+    alert("Inserisci solo numeri!");
+    i--;
+  } else {
+    somma += Number(numero);
+  }
+}
+console.log(`La somma dei numeri inseriti è ${somma}.`);
