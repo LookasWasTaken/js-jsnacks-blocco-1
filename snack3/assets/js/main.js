2512e6eed1fe20582639ccdb191a3a8bb1ace31d
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-15 11:56:29 +0100

    add while loop

--- snack3/assets/js/main.js
+++ snack3/assets/js/main.js
@@ -1,3 +1,5 @@
+// For Loop
+
 let somma = 0;
 for (let i = 1; i <= 10; i++) {
   let numero = Number(prompt(`Inserisci il numero ${i}:`));
@@ -10,3 +12,18 @@ for (let i = 1; i <= 10; i++) {
   }
 }
 console.log(`La somma dei numeri inseriti è ${somma}.`);
+
+// While Loop
+
+let j = 1;
+while (j <= 10) {
+  let numero = Number(prompt(`Inserisci il numero ${j}:`));
+  somma += numero;
+  console.log(`Il numero scritto è ${numero}`);
+  console.log(`La somma attuale è ${somma}`);
+  if (isNaN(numero)) {
+    alert("Inserisci solo numeri!");
+    j--;
+  }
+  j++;
+}
